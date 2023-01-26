6e0b7026ab423daa07e7f0ee1d91bb45ac9abe80
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-01-26 14:29:24 -0500

    Add Factory For Players/Modules For Game

--- app.js
+++ app.js
@@ -1,54 +1,80 @@
-// Create a way to make player objects
-const Players = (name, score) => {
+const Players = (name, score, marker) => {
+
+  const getScore = () => score;
+  const getName = () => name;
   return {
     name,
-    score
-  };
+    score,
+    marker,
+    getScore,
+    getName
+  }
 };
 
-// create an object for interfacing with a board
-const GameBoard = (() => {
-  const gameBoard = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', 'O'];
-  const board = document.getElementById('gameBoard')
-  const _createDivs = () => {
-    for (let i = 0; i <= 2; i++) {
-      const divHolder = document.createElement('div');
-      divHolder.classList.add('row');
 
-      board.appendChild(divHolder);
-    }
+//Where we update information, scores, and such
+
+
+const GameBoard = (function GameBoard() {
+  const boardArray = ["X", "O", null, null, null, null, "O", null, null];
+
+  const getBoard = () => boardArray;
+
+  const placeMarker = (index, marker) => {
+    if (boardArray[index] !== null) {
+      return
+    };
+    boardArray[index] = marker;
   }
-  _createDivs();
-  const _createButtons = (array) => {
-    for (let i = 0; i < array.length; i++) {
-      const createButton = document.createElement('button');
-      createButton.classList.add('square');
-      // This was will go through array, what do we want it to do or happen?
-      // Well we want to essentially make buttons base on length
-      // then, append those buttons to board container
-      // Hopefully it works
-      board.appendChild(createButton);
-    }
-  }
-  const displayButtons = () => {
-    _createButtons(gameBoard);
-  }
+
+
   return {
-    gameBoard,
-    displayButtons
-  };
+    getBoard,
+    placeMarker,
+
+  }
 })();
 
-// tie these thing together to control flow of the game in another object
-const controlFlowOfGame = () => {
-  const player1 = Players('player1', 0);
-  const player2 = Players('player2', 0);
-  const board = GameBoard;
+const ScreenController = (function ScreenController() {
+  const board = document.querySelector('.gameBoard');
+  board.addEventListener('click', (e) => {
+    console.log(e.target.dataset.number);
+  })
+})();
 
-  board.displayButtons();
+
+const Game = (() => {
+  const player1 = Players("Player 1", 0, "X");
+  const player2 = Players("Player 2", 0, "O");
+
+  let activePlayer = player1;
+
+  const _switchPlayerTurn = () => {
+    if (activePlayer === player1) {
+      activePlayer = player2;
+    } else {
+      activePlayer = player1;
+    }
+  }
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+
+    console.log(`${getActivePlayer().name}'s turn`);
+  }
+
+  const playRound = () => {
+    _switchPlayerTurn();
+    printNewRound();
+  }
+
+  printNewRound();
+
   return {
-    controlFlowOfGame
-  };
-}
+    playRound,
+    getActivePlayer
+  }
+})();
 
-controlFlowOfGame();
+Game.playRound();
